fea284831f92106973aa69d0ff5c08635a3c8f05
Nacho <isanccal@myuax.com>
2024-11-19 22:50:39 +0100

    cambios

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -6,8 +6,6 @@ def index(request):
     return render(request, "core/index.html")
 def about(request):
     return render(request, "core/about.html")
-def services(request):
-    return render(request, "core/services.html")
 def store(request):
     return render(request, "core/store.html")
 def contact(request):
