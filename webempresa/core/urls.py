b2a7316f6b5c4c621b8922be8bc1fd9cb7a4cc4e
Nacho <isanccal@myuax.com>
2024-11-19 17:55:11 +0100

    cambio de template han ido bien

--- webempresa/core/urls.py
+++ webempresa/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('services/', views.services, name="services"),
     path('store/', views.store, name="store"),
