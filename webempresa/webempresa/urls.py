26fadcf8e690cc5fc3923f2e06b26d59aace871d
Nacho <isanccal@myuax.com>
2024-11-20 18:59:02 +0100

    no se como ponerlo en español lo del blog

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('', include('core.urls')),
     path('services/', include('services.urls')),
-    path('blog/', include('blog.urls')),
     path("admin/", admin.site.urls),
 ]
 
