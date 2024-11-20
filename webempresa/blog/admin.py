9166f01c3717e79c7a25c84bca12f9fee7ef2f6a
Nacho <isanccal@myuax.com>
2024-11-20 19:44:37 +0100

    v

--- webempresa/blog/admin.py
+++ webempresa/blog/admin.py
@@ -5,6 +5,10 @@ class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published')
+    ordering = ('author', 'published')
+    search_fields = ('title','content','author__username',
+ 'categories__name')
 
 admin.site.register(Category, CategoryAdmin)
 
