cf45c28b7b214d9e58b1ea027723845b218b93d3
Nacho <isanccal@myuax.com>
2024-11-22 11:22:58 +0100

    creando category.htm

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -6,3 +6,8 @@ from .models import Post, Category
 def blog(request):
     posts = Post.objects.all()
     return render(request, 'blog/blog.html', {'posts':posts})
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, "blog/category.html",
+{'category':category})
