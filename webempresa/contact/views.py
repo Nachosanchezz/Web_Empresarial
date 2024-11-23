cdc582994c53939404223637b7362d4e62d88a63
Nacho <isanccal@myuax.com>
2024-11-23 15:51:02 +0100

    a ver si llega el correo

--- webempresa/contact/views.py
+++ webempresa/contact/views.py
@@ -18,7 +18,7 @@ def contact(request):
                 subject="La Caffettiera: Nuevo mensaje de contacto",  # Asunto
                 body=f"De {name} <{email}>\n\nEscribió:\n\n{content}",  # Cuerpo
                 from_email=EMAIL_HOST_USER,  # Email de origen
-                to=["destino@example.com"],  # Cambia por el email destino
+                to=["isanccal@myuax.com"],  # Cambia por el email destino
                 reply_to=[email]  # Email de respuesta
             )
 
