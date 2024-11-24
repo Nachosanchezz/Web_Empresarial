370ae16bfb89eb943f37996c799f14102dd11185
Nacho <isanccal@myuax.com>
2024-11-24 14:27:16 +0100

    cambios

--- webempresa/pages/models.py
+++ webempresa/pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.text import slugify
 
 
 class Page(models.Model):
@@ -17,3 +18,8 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Page, self).save(*args, **kwargs)
--- webempresa/pages/urls.py
+++ webempresa/pages/urls.py
@@ -4,8 +4,4 @@ from . import views
 
 urlpatterns = [
     path('<int:page_id>/<slug:page_slug>/', views.page, name="page"),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> parent of 2308a95 (a ver si va la politica de privacidad)
