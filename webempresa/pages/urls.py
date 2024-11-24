585198d8910c45f620b46ad40c72f3ad36181860
Nacho <isanccal@myuax.com>
2024-11-24 14:23:09 +0100

    CAMVIO

--- webempresa/pages/urls.py
+++ webempresa/pages/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('<int:page_id>/<slug:page_slug>/', views.page, name="page"),
+<<<<<<< HEAD
 ]
+=======
+]
+>>>>>>> parent of 2308a95 (a ver si va la politica de privacidad)
