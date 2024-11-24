5cda2f3c9864a27bb97abd4b65f97f7350290672
Nacho <isanccal@myuax.com>
2024-11-24 18:46:41 +0100

    ahora si que si fin de la cafeteroa

--- webempresa/core/urls.py
+++ webempresa/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('', views.index, name="index"),
+    path('store.html', RedirectView.as_view(url='store/')),
     path('about/', views.about, name="about"),
     path('store/', views.store, name="store"),
     path('sample/', views.sample, name="sample"),
